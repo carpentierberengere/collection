90955eac0df610e192413c6839a52cf96eaec807
Bérengère Carpentier <72983609+carpentierberengere@users.noreply.github.com>
2020-11-10 16:52:19 +0100

    java

---
+++ collection.js
@@ -0,0 +1,73 @@
+const collection = [
+    { //-----------------------------------------SHINING-------------------------------//
+        title: 'The Shining',
+        author: 'Stanley Kubrick',
+        category: ['Psycho','Horror'],
+        link: 'https://www.youtube.com/watch?v=5Cb3ik6zP2I',
+        description: 'Jack and his family move into an isolated hotel with a violent past. Living in isolation, Jack begins to lose his sanity, which affects his family members.',
+    },
+    { //-------------------------------------BACK TO THE FUTURE-----------------------//
+        title: 'Back To The Future',
+        author: 'Robert Zemeckis',
+        category: ['Sci-Fi'],
+        link: 'https://www.youtube.com/watch?v=qvsgGtivCgs',
+        description: 'Marty travels back in time using an eccentric scientists time machine. However, he must make his high-school-aged parents fall in love in order to return to the present.',
+    },
+    { //------------------------------------STAR WARS---------------------------------//
+        title: 'Star Wars EpIV: A New Hope',
+        author: 'George Lucas',
+        category: ['Space-Opera','Sci-Fi'],
+        link: 'https://www.youtube.com/watch?v=1g3_CFmnU7k',
+        description: 'After Princess Leia, the leader of the Rebel Alliance, is held hostage by Darth Vader, Luke and Han Solo must free her and destroy the powerful weapon created by the Galactic Empire',
+    },
+    { //------------------------------------CHARLIE & THE CHOCOLATE FACTORY------------//
+        title: 'Charlie & The Chocolate Factory',
+        author: 'Tim Burton',
+        category: ['Fantasy','Children'],
+        link: 'https://www.youtube.com/watch?v=OFVGCUIXJls',
+        description: 'Charlie, a young boy from an impoverished family, and four other kids win a tour of an amazing chocolate factory run by an imaginative chocolatier, Willy Wonka, and his staff of Oompa-Loompas',
+    },
+    { //------------------------------------AMERICAN MURDER-----------------------------//
+        title: 'Americaan Murder: The Family Next Door',
+        author: 'NETFLIX',
+        category: ['Murder','Documentary'],
+        link: 'https://www.youtube.com/watch?v=ep8iKiQNSrY',
+        description: 'American Murder: The Family Next Door is an American true crime documentary film directed by Jenny Popplewell. The film tells the story of the 2018 Watts family murders, which took place in Frederick, Colorado',
+    },
+    { //----------------------------------BOLEYN GIRL----------------------------------//
+        title: 'The Other Boleyn Girl',
+        author: 'Justin Chadwick',
+        category: ['Historic','Drama'],
+        link: 'https://www.youtube.com/watch?v=M44ZHmPoLSU',
+        description: 'Centred on the the court of Henry VIII, this historical drama presents a calculating Anne, who usurps her older sister Mary as the kings mistress and ultimately desires to be his new queen',
+    },
+    { //---------------------------------SPIRITED AWAY--------------------------------//
+        title: 'Spirited Away',
+        author: 'Hayao Miyazaki',
+        category: ['Anime','Fantasy'],
+        link: 'https://www.youtube.com/watch?v=ByXuk9QqQkk',
+        description: 'Hayao Miyazaki, 10-year-old Chihiro (Rumi Hiiragi) and her parents (Takashi Naitô, Yasuko Sawaguchi) stumble upon a seemingly abandoned amusement park. After her mother and father are turned into giant pigs, Chihiro meets the mysterious Haku (Miyu Irino), who explains that the park is a resort for supernatural beings who need a break from their time spent in the earthly realm, and that she must work there to free herself and her parents.',
+    },
+    { //--------------------------------SOCIAL DILEMMA-------------------------------//
+        title: 'The Social Dilemma',
+        author:'NETFLIX',
+        category: ['Society','Documentary'],
+        link:'https://www.youtube.com/watch?v=uaaC57tcci0',
+        description: 'Tech experts sound the alarm on the dangerous human impact of social networking',
+    },
+    { //--------------------------------MEANING OF LIFE-----------------------------//
+        title: 'The Meaning Of Life',
+        author: 'Monty Python',
+        category: ['Satire','Comedy'],
+        link: 'https://www.youtube.com/watch?v=vabfV4HRvRY',
+        description: 'The stages of life are told through multiple sketches and songs by the British comedy troupe. The seven parts of life cover birth, growing up, war, middle age, organ transplants, old age and death. Not all stages are singular: "Part I: The Miracle of Birth" is from the perspective of an ignored woman in labor, and of a Roman Catholic couple with too many children, and "Part VII: Death" encompasses a funeral and heaven. Added are three unrelated skits placed in the beginning, middle and end',
+    },
+    { //------------------------------------LIFE---------------------------------//
+        title: 'LIFE',
+        author: 'Daniel Espinosa',
+        category: ['Sci-Fi','Horror'],
+        link: 'https://www.youtube.com/watch?v=cuA-xqBw4jE',
+        description:'As astronauts discover the first evidence of extra-terrestrial life on Mars, they begin realising that the life form is extremely intelligent and hostile',
+    }
+
+]
